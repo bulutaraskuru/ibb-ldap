bd8721d217899fa0d872d998717b1b1bae4a8f1a
bulut.kuru <bulut.kuru@ibb.gov.tr>
2024-02-29 14:49:48 +0300

    version update  21

--- src/Traits/ExtractsFieldsFromTable.php
+++ src/Traits/ExtractsFieldsFromTable.php
@@ -9,15 +9,13 @@ trait ExtractsFieldsFromTable
     /**
      * Retrieve table fields and set them as fillable in the model.
      */
-    public static function bootExtractsFieldsFromTable()
+    protected function initializeExtractsFieldsFromTable()
     {
-        static::retrieved(function ($model) {
-            if (Schema::hasTable($model->getTable())) {
-                $columns = Schema::getColumnListing($model->getTable());
-                // Filter out unwanted fields
-                $columns = array_diff($columns, ['id', 'created_at', 'updated_at', 'deleted_at']);
-                $model->fillable = array_merge($model->getFillable(), $columns);
-            }
-        });
+        if (Schema::hasTable($this->getTable())) {
+            $columns = Schema::getColumnListing($this->getTable());
+            // Filter out unwanted fields
+            $columns = array_diff($columns, ['id', 'created_at', 'updated_at', 'deleted_at']);
+            $this->fillable = array_merge($this->getFillable(), $columns);
+        }
     }
 }
